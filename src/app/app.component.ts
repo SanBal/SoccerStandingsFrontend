a97dd4ccc067b1facca637c454517e1902457660
Sanjeev <sanjeev.b@gmx.de>
2018-01-20 16:44:26 +0100

    Add margin to main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template:
   ` <app-header></app-header>
-    <router-outlet></router-outlet>
+    <div class="content">
+      <router-outlet></router-outlet>
+    </div>
   `,
   styleUrls: ['./app.component.css']
 })
