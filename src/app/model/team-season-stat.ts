120dee7e3f0a7a5c197c9456f270eeb74750e601
Sanjeev <sanjeev.b@gmx.de>
2018-01-25 16:33:10 +0100

    Add more attributes to team season statistic model Add more infos to season standings Add bootstrap column widths to table columns Center table cells text

--- src/app/model/team-season-stat.ts
+++ src/app/model/team-season-stat.ts
@@ -2,13 +2,21 @@ import { Team } from './team';
 export class TeamSeasonStat {
   team: Team;
   position: number;
+  numMatches: number;
   points: number;
+  wins: number;
+  losses: number;
+  draws: number;
   goalDifference: number;
 
   constructor(team: Team, teamStandingInfo: any) {
     this.team = team;
     this.position = teamStandingInfo.position;
+    this.numMatches = teamStandingInfo.playedGames;
     this.points = teamStandingInfo.points;
+    this.wins = teamStandingInfo.wins;
+    this.losses = teamStandingInfo.losses;
+    this.draws = teamStandingInfo.draws;
     this.goalDifference = teamStandingInfo.goalDifference;
   }
 }
