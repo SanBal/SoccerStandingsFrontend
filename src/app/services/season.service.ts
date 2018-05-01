43f8f6ce949260c92a8f5d92d0c43c42afc587e6
Sanjeev <sanjeev.b@gmx.de>
2018-05-01 12:11:26 +0200

    Use promise object for handling asynch external football standings api call
    Add link to Font Awesome icons website

--- src/app/services/season.service.ts
+++ src/app/services/season.service.ts
@@ -8,25 +8,38 @@ import {Team} from '../model/team';
 @Injectable()
 export class SeasonService {
 
-  stats: TeamSeasonStat[] = [];
+  public static readonly JSON_STANDING_KEY = 'standing';
   constructor(private http: HttpClient) {}
 
-  getTeamSeasonStats() {
-    console.log('Season Service:');
+  /**
+   * Returns a promise object with 'TeamSeasonStat' array
+   * if the HTTP-GET call to the ' football-data' API was successful
+   */
+  getPromiseTeamSeasonStats(): Promise<any> {
+    console.log('Season Service - GET leagueTable standings');
     const headers = new HttpHeaders().set('X-Auth-Token', 'd4bcc5971ac24789addad53e8e1a968d');
-    this.http.get('http://api.football-data.org//v1/competitions/455/leagueTable', {headers})
-      .subscribe(resp => {
-        console.log('response:');
-        console.log(resp);
-        for (let i = 0; i < Object.keys(resp['standing']).length; i++) {
-          const standing: any = resp['standing'][i];
-          const teamURI: string = standing._links.team.href;
-          const id: number = +teamURI.substr(teamURI.lastIndexOf('/') + 1, teamURI.length);
-          const team = new Team(id, standing);
-          const stat = new TeamSeasonStat(team, standing);
-          this.stats.push(stat);
-        }
+    return this.http.get('http://api.football-data.org//v1/competitions/455/leagueTable', {headers})
+      .toPromise() // Convert 'Observable' HTTP response to 'Promise' object
+      .then(this.getExtractedTeamSeasonStats) // Append callback function which is called if request was successful
+      .catch(error => {
+        // Return error promise with response error object
+        throw Promise.reject(error);
       });
-    return this.stats;
+  }
+
+  private getExtractedTeamSeasonStats(res: Response) {
+    const stats: TeamSeasonStat[] = [];
+    console.log('GET leagueTable standings response:');
+    console.log(res);
+
+    for (let i = 0; i < Object.keys(res[SeasonService.JSON_STANDING_KEY]).length; i++) {
+      const standing: any = res[SeasonService.JSON_STANDING_KEY][i];
+      const teamURI: string = standing._links.team.href;
+      const id: number = +teamURI.substr(teamURI.lastIndexOf('/') + 1, teamURI.length);
+      const team = new Team(id, standing);
+      const stat = new TeamSeasonStat(team, standing);
+      stats.push(stat);
+    }
+    return stats;
   }
 }
--- src/app/views/soccer-standings/soccer-standings.component.ts
+++ src/app/views/soccer-standings/soccer-standings.component.ts
@@ -2,6 +2,7 @@ import {Team} from '../../model/team';
 import {TeamSeasonStat} from '../../model/team-season-stat';
 import {SeasonService} from '../../services/season.service';
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-team-standings',
@@ -10,18 +11,31 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SoccerStandingsComponent implements OnInit {
 
-  stats: TeamSeasonStat[] = [];
-  constructor(private seasonService: SeasonService) {}
+  private loading = false;
+  teamSeasonStats: TeamSeasonStat[] = [];
+  constructor(private seasonService: SeasonService, private router: Router) {}
 
   ngOnInit() {
     const useMockObjects = false;
     if (useMockObjects) {
       this.setMockObjects();
     } else {
-      this.stats = this.seasonService.getTeamSeasonStats();
+      this.loading = true;
+
+      // Consume promise of Season Service
+      this.seasonService.getPromiseTeamSeasonStats()
+        .then(teamSeasonStats => {
+          this.teamSeasonStats = teamSeasonStats;
+          this.loading = false;
+        }).catch(error => {
+          console.log(error);
+          setTimeout(_ => {
+            this.router.navigate(['page-not-found']);
+          }, 5000);
+        });
     }
-    console.log('stats');
-    console.log(this.stats);
+    console.log('TeamSeasonStats:');
+    console.log(this.teamSeasonStats);
   }
 
   setMockObjects() {
@@ -30,7 +44,7 @@ export class SoccerStandingsComponent implements OnInit {
       {'position': 1, 'points': 10, 'goalDifference': 20},
       {'position': 2, 'points': 5, 'goalDifference': 10}
     ];
-    this.stats.push(new TeamSeasonStat(new Team(1, teamsInfo[0]), standingsInfo[0]));
-    this.stats.push(new TeamSeasonStat(new Team(2, teamsInfo[1]), standingsInfo[1]));
+    this.teamSeasonStats.push(new TeamSeasonStat(new Team(1, teamsInfo[0]), standingsInfo[0]));
+    this.teamSeasonStats.push(new TeamSeasonStat(new Team(2, teamsInfo[1]), standingsInfo[1]));
   }
 }
